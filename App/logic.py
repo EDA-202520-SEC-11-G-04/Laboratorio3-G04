00137743f53808a86d46f63f6ccaecccc81121ed
basto34 <bastien.thirion@eleves.ec-nantes.fr>
2025-08-23 23:34:49 -0500

    Entrega Final Laboratorio 3

--- App/logic.py
+++ App/logic.py
@@ -30,6 +30,7 @@ import time
 
 # TODO Importar la librería para el manejo de listas
 from DataStructures.List import array_list as lt
+
 data_dir = os.path.dirname(os.path.realpath('__file__')) + '/Data/'
 
 """
@@ -53,8 +54,13 @@ def new_logic():
 
     catalog['books'] = lt.new_list()
     # TODO Implemente la inicialización de la lista de autores
+        
     # TODO Implemente la inicialización de la lista de tags
     # TODO Implemente la inicialización de la lista de asociación de libros y tags
+    catalog['books'] = lt.new_list()
+    catalog['authors'] = lt.new_list()    
+    catalog['tags'] = lt.new_list()         
+    catalog['book_tags'] = lt.new_list()
     return catalog
 
 
@@ -71,9 +77,11 @@ def load_data(catalog):
     # TODO Complete la carga de los tags
     # TODO Complete la carga de los book_tags
     # TODO Añada los parámetros de retoro necesarios
+    tags = load_tags(catalog)           
+    book_tags = load_books_tags(catalog)  
     end_time = getTime()
     tiempo_transcurrido = deltaTime(end_time, start_time)
-    return books, authors, tiempo_transcurrido
+    return books, authors,tags, book_tags, tiempo_transcurrido
 
 
 
@@ -99,7 +107,12 @@ def load_tags(catalog):
     :return: El número de tags cargados
     """
     # TODO Implementar la carga de los tags
-    pass
+    tagsfile = data_dir + 'GoodReads/tags.csv'
+    input_file = csv.DictReader(open(tagsfile, encoding='utf-8'))
+    for tag in input_file:
+        add_tag(catalog, tag)
+    return tag_size(catalog)
+
 
 
 def load_books_tags(catalog):
@@ -111,6 +124,11 @@ def load_books_tags(catalog):
     :return: El número de book_tags cargados
     """
     # TODO Implementar la carga de los book_tags
+    booktagsfile = data_dir + 'GoodReads/book_tags.csv'
+    input_file = csv.DictReader(open(booktagsfile, encoding='utf-8'))
+    for book_tag in input_file:
+        add_book_tag(catalog, book_tag)
+    return book_tag_size(catalog)
     pass
 
 
@@ -139,6 +157,12 @@ def get_best_book(catalog):
     start_time = getTime()
     best_book = None
     # TODO Implementar la función del mejor libro por rating
+    current = catalog['books']['elements'] 
+    if lt.size(catalog['books']) > 0:
+        best_book = current[0]
+        for book in current:
+            if float(book['average_rating']) > float(best_book['average_rating']):
+                best_book = book
     end_time = getTime()
     tiempo_transcurrido = deltaTime(end_time, start_time)
     return best_book, tiempo_transcurrido
@@ -156,6 +180,13 @@ def count_books_by_tag(catalog, tag):
     start_time = getTime()
     resultado = 0
     # TODO Implementar la función de conteo de libros por tag
+    for bt in catalog['book_tags']['elements']:
+        # buscamos el nombre del tag correspondiente
+        pos = lt.is_present(catalog['tags'], tag, compare_tag_names)
+        if pos > 0:
+            tag_elem = lt.get_element(catalog['tags'], pos)
+            if bt['tag_id'] == tag_elem['tag_id']:
+                resultado += 1
     end_time = getTime()
     tiempo_transcurrido = deltaTime(end_time, start_time)
     return resultado, tiempo_transcurrido
@@ -254,7 +285,8 @@ def author_size(catalog):
     :return: El número de autores en el catálogo
     """
     # TODO Implementar la función de tamaño de autores
-    pass
+    return lt.size(catalog['tags'])
+
 
 
 def tag_size(catalog):
@@ -266,7 +298,8 @@ def tag_size(catalog):
     :return: El número de tags en el catálogo
     """
     # TODO Implementar la función de tamaño de tags
-    pass
+    return lt.size(catalog['tags'])
+    
 
 
 def book_tag_size(catalog):
@@ -278,7 +311,7 @@ def book_tag_size(catalog):
     :return: El número de book_tags en el catálogo
     """
     # TODO Implementar la función de tamaño de book_tags
-    pass
+    return lt.size(catalog['book_tags'])
 
 
 # Funciones utilizadas para comparar elementos dentro de una lista
--- DataStructures/List/array_list.py
+++ DataStructures/List/array_list.py
@@ -36,3 +36,49 @@ def size(my_list):
 
 def first_element(my_list):
     return my_list["elements"][0]
+
+def is_empty(my_list):
+    empty=False
+    if my_list["size"]==0:
+        empty=True
+    return empty
+        
+def last_element(my_list):
+    return my_list["elements"][size(my_list)-1]
+
+def delete_element(my_list, pos):
+    my_list["elements"].pop(pos)
+    my_list["size"]-=1
+    return my_list
+    
+    
+def remove_first(my_list):
+    val=my_list["elements"].pop(0)
+    my_list["size"]-=1
+    return val
+
+def remove_last(my_list):
+    val=my_list["elements"].pop()
+    my_list["size"]-=1
+    return val
+ 
+def insert_element(my_list, element, pos):
+    my_list["elements"].insert(element,pos)
+    my_list["size"]+=1
+    return my_list
+
+def change_info(my_list, pos, new_info):
+    my_list["elements"][pos]=new_info
+    return my_list
+
+def exchange(my_list, pos1, pos2):
+    x=my_list["elements"][pos1]
+    my_list["elements"][pos1]=my_list["elements"][pos2]
+    my_list["elements"][pos2]=x
+    return my_list
+
+def sub_list(my_list, pos_i, num_elements):
+    sub=new_list()
+    sub["size"]=num_elements
+    sub["elements"]=my_list["elements"][pos_i:pos_i+num_elements]
+    return sub
